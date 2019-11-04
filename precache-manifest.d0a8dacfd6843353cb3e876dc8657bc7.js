0ceddc5c9f5ce1abbf0d737b09bcf74a9a63da2c
barbie-ez <barbie.ezomo@gmail.com>
2019-11-04 23:47:25 +0100

    Updates

---
+++ precache-manifest.d0a8dacfd6843353cb3e876dc8657bc7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0423666b31c2ebba20324d3528b431c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c5bafad1c4a5ce1ed43b",
+    "url": "/portfolio/static/css/2.559fd73f.chunk.css"
+  },
+  {
+    "revision": "f230db0bd35db58ea956",
+    "url": "/portfolio/static/css/main.c9e7d971.chunk.css"
+  },
+  {
+    "revision": "c5bafad1c4a5ce1ed43b",
+    "url": "/portfolio/static/js/2.e16e51ee.chunk.js"
+  },
+  {
+    "revision": "f230db0bd35db58ea956",
+    "url": "/portfolio/static/js/main.60f50d58.chunk.js"
+  },
+  {
+    "revision": "76aa8cbd67b5c937ec2c",
+    "url": "/portfolio/static/js/runtime-main.e92c632a.js"
+  },
+  {
+    "revision": "e270cd7cbd947a107cb053f2cbd3ba39",
+    "url": "/portfolio/static/media/photo.e270cd7c.jpg"
+  }
+]);
